a9e659146099c7479116795e471d23a92e7115f7
pavel <pavelkononenko1@gmail.com>
2017-01-28 22:02:40 +0300

    book-list on the main page

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default  function(){
+    return [
+        {title: 'Alice in wonderland', count : 5},
+        {title: 'Buggers', count : 13},
+        {title: 'Blask swan', count : 23},
+        {title: 'Taxy', count : 6}
+    ];
+}
